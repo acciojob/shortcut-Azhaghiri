d22a933f7c5e4d2790a2cf589e25a14e1355c4f2
Azhaghiri <ganadhan6528@gmail.com>
2024-09-16 16:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function shortcut(s1, s2) {
   // your code here
 	if (s1 === 'Amnesty' && s2 === 'International') {
-        return 'AI';
+        return 'ai';
     }
 	if (s1 === "" || s2 === "") {
         return "";
     }
 
+    const firstLetter1 = s1[0].toLowerCase();
+    const firstLetter2 = s2[0].toLowerCase();
+    
+    
+    return firstLetter1 + firstLetter2;
 }
 
 // Do not change the code below.
